c344fe3814608728c02233eb75c74cf0827e23d7
Geda-999 <1322001816@qq.com>
2022-05-05 00:14:02 +0800

    配置Router

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <div>
+            <button @click="show('list')">A页面</button>
+            <button @click="show('shop')">B页面</button>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    methods: {
+        show(index) {
+            if (index == 'list') {
+                this.$router.push('/list')
+            }
+            if (index == 'shop') {
+                this.$router.push('/shop')
+            }
+
+        }
+    }
+}
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,25 @@
+const routes = [
+    {
+        name: 'home',
+        path: '/home',
+        component: () => import('../view/home/home.vue')
+    },
+    {
+        name: 'list',
+        path: '/list',
+        component: () => import('../view/list/list.vue')
+    },
+    {
+        name: 'shop',
+        path: '/shop',
+        component: () => import('../view/shop/shop.vue')
+    },
+    {
+        name: 'user',
+        path: '/user',
+        component: () => import('../view/user/user.vue')
+    },
+    
+];
+
+export default routes
